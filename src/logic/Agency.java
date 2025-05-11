52a1f194751c2f95a0c1bbba8ff4708578781784
Martín Alejandro García <martinalejandrogarciababastro@gmail.com>
2025-05-11 03:02:18 -0400

    feat: refresh Agency class

--- src/logic/Agency.java
+++ src/logic/Agency.java
@@ -1,5 +1,56 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Agency {
 
+	private String name;
+	private ArrayList<Company> companies;
+	private ArrayList<Candidate> candidates;
+	private ArrayList<MonthRegister> mRegisteres;
+
+	public Agency(String name){
+		companies = new ArrayList<Company>();
+		candidates = new ArrayList<Candidate>();
+		mRegisteres = new ArrayList<MonthRegister>();
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (!(name.isEmpty()))
+			this.name = name;
+		else
+			throw new IllegalArgumentException("The name cannot be empty");
+
+	}
+
+	public ArrayList<Company> getCompanies() {
+		return companies;
+	}
+
+	public void addCompany(Company c) {
+		companies.add(c);
+	}
+
+	public ArrayList<Candidate> getCandidates() {
+		return candidates;
+	}
+
+	public void addCandidate(Candidate c) {
+		candidates.add(c);
+	}
+
+	public ArrayList<MonthRegister> getmRegisteres() {
+		return mRegisteres;
+	}
+
+	public void addmRegisteres(MonthRegister m) {
+		mRegisteres.add(m);
+	}
+
 }
+
