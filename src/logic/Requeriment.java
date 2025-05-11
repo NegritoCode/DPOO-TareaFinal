943949c076c671651f58716fcfa3fdae0afea5c5
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-11 03:39:04 -0400

    feat: add Requeriment class

--- src/logic/Requeriment.java
+++ src/logic/Requeriment.java
@@ -1,5 +1,20 @@
 package logic;
 
-public class Requeriment{
+public class Requeriment {
+    private String type;
+    private String data;
 
+    public Requeriment(String type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+    
+    // Getters y setters
+    public String getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
