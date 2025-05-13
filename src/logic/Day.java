fdb2ab2530e999edea5ce834bb8663e2a97c7181
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-12 22:08:32 -0400

    feat: add createInterview method in Day && private some setters

--- src/logic/Day.java
+++ src/logic/Day.java
@@ -3,20 +3,55 @@ package logic;
 import java.util.ArrayList;
 
 public class Day {
-	private String id;
-	private String monthId;
-	private ArrayList<Interview> interviews;
-	
-	public Day(String id, String monthId) {
-		setId(id);
-		setMonthId(monthId);
-	}
+    private String id;
+    private String monthId;
+    private ArrayList<Interview> interviews;
 
-	public String getId() {
-		return id;
-	}
+    public Day(String id, String monthId) {
+        setId(id);
+        setMonthId(monthId);
 
-	public void setId(String id) {
+        this.interviews = new ArrayList<Interview>();
+    }
+
+    /**
+     * Añade una entrevista si no existe el mismo candidato
+     */
+    public void createInterview(String candidateCid, String companyId, String offerId) {
+        if (getInterview(candidateCid) == null) {
+            interviews.add(new Interview(monthId, id, candidateCid, companyId, offerId));
+        } else {
+            throw new IllegalStateException("Ya existe una entrevista asignada en este día para " + candidateCid);
+        }
+
+    }
+
+    /**
+     * Obtener la entrevista segun el cid del candidato
+     * Retorna null si no se encuentra
+     */
+    public Interview getInterview(String candidateCid) {
+        Interview founded = null;
+        int i = 0;
+
+        while (founded == null && i < interviews.size()) {
+            Interview interview = interviews.get(i);
+            if (interview.getCandidateCid() == candidateCid) {
+                // se encontró la entervista!
+                founded = interview;
+            }
+            i++;
+        }
+
+        return founded;
+    }
+
+    // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+	private void setId(String id) {
 		this.id = id;
 	}
 
@@ -24,16 +59,11 @@ public class Day {
 		return monthId;
 	}
 
-	public void setMonthId(String monthId) {
+	private void setMonthId(String monthId) {
 		this.monthId = monthId;
 	}
 
-	public ArrayList<Interview> getInterviews() {
-		return interviews;
-	}
-
-	public void setInterviews(ArrayList<Interview> interviews) {
-		this.interviews = interviews;
-	}
+    public ArrayList<Interview> getInterviews() {
+        return interviews;
+    }
 }
-
