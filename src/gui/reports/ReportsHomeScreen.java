c9592a199cc0010ec203643bcce55fa5f6e782e1
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-06-23 09:33:39 -0400

    feat: add search and filtering in Reports

--- src/gui/reports/ReportsHomeScreen.java
+++ src/gui/reports/ReportsHomeScreen.java
@@ -1,6 +1,9 @@
 package gui.reports;
 
+import gui.components.FilterDialog;
+import gui.components.MButton;
 import gui.components.MTable;
+import gui.components.SearchField;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,12 +18,16 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.border.LineBorder;
 
 import utils.Navigation;
+import utils.constants.Branch;
+import utils.constants.Sector;
+import utils.constants.Specialty;
 
 import javax.swing.SwingConstants;
 
@@ -34,20 +41,135 @@ import logic.interview.Interview;
 import javax.swing.JTextField;
 import javax.swing.ImageIcon;
 
+import gui.reports.ReportPanel;
+
 public class ReportsHomeScreen extends JFrame {
 	private static final long serialVersionUID = 1L;
-
+	final Agency agency = GlobalAgency.getInstance();
 	private JPanel contentPane;
 	private DefaultTableModel tableModel;
+	private ReportPanel panelReport1, panelReport2, panelReport3, panelReport4;
 
-	private MTable table;
-	private JTextField btnSearch;
+	private void initializePanels() {
+		panelReport1 = new ReportPanel("Mejores Ofertas", Branch.BRANCHES,
+				new String[] { "Salario", "Rama", "Compañía" }, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String query = panelReport1.getSearchText();
+						HashMap<String, Boolean> filter = panelReport1.getFilterOptions();
+						DefaultTableModel table = panelReport1.getTableModel();
+
+						table.setRowCount(0);
+						for (Offer offer : agency.getBestOffers()) {
+							Company company = agency.getCompanyManager().getCompanyById(offer.getCompanyId());
+							if ((company.getName().toLowerCase().contains(query)) && filter.get(offer.getBranch())) {
+								table.addRow(
+										new Object[] { offer.getSalary() + "", offer.getBranch(), company.getName() });
+							}
+						}
+					}
+				});
+
+		// obtener lista de nombres de candidatos para el filtro
+		ArrayList<Candidate> candidates = agency.getCandidates();
+		String[] names = new String[candidates.size()];
+		for (int i = 0; i < candidates.size(); i++) {
+			names[i] = candidates.get(i).getName();
+		}
+		panelReport2 = new ReportPanel("Entrevistas por día", names,
+				new String[] { "Candidato", "Mes", "Día", "Oferta" }, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String query = panelReport2.getSearchText();
+						HashMap<String, Boolean> filter = panelReport2.getFilterOptions();
+						DefaultTableModel table = panelReport2.getTableModel();
+
+						table.setRowCount(0);
+						for (Candidate candidate : candidates) {
+							for (Interview interview : candidate.getInterviews()) {
+								if ((candidate.getName().toLowerCase().contains(query)
+										|| interview.getMonthlyId().contains(query))
+										&& filter.get(candidate.getName())) {
+									table.addRow(new Object[] { candidate.getName(), interview.getMonthlyId(),
+											interview.getDayId(), interview.getOfferId() });
+								}
+							}
+						}
+					}
+				});
+
+		panelReport3 = new ReportPanel("Mayores Empleadores", Sector.SECTORS,
+				new String[] { "Nombre", "Dirección", "Teléfono", "Sector" }, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String query = panelReport3.getSearchText();
+						HashMap<String, Boolean> filter = panelReport3.getFilterOptions();
+						DefaultTableModel table = panelReport3.getTableModel();
+						
+						table.setRowCount(0);
+						for (Company c : agency.getCompaniesMostOffers()) {
+							if ((c.getName().toLowerCase().contains(query)
+									|| c.getAddress().toLowerCase().contains(query)
+									|| c.getPhone().toLowerCase().contains(query)) && filter.get(c.getSector())) {
+								table.addRow(new Object[] { c.getName(), c.getAddress(), c.getPhone(), c.getSector() });
+							}
+						}
+					}
+				});
+
+		// Fusionar los arreglos
+		String[] mergedArray = new String[Specialty.specialties.length + Branch.BRANCHES.length];
+		System.arraycopy(Specialty.specialties, 0, mergedArray, 0, Specialty.specialties.length);
+		System.arraycopy(Branch.BRANCHES, 0, mergedArray, Specialty.specialties.length, Branch.BRANCHES.length);
+		panelReport4 = new ReportPanel("Candidatos Destacados", mergedArray, new String[] { "Cid", "Rama", "Nombre",
+				"Sexo", "Dirección", "Teléfono", "Escolaridad", "Especialidad", "Experiencia" }, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String query = panelReport4.getSearchText();
+						HashMap<String, Boolean> filter = panelReport4.getFilterOptions();
+						DefaultTableModel table = panelReport4.getTableModel();
+						
+						table.setRowCount(0);
+						for (Candidate c : GlobalAgency.getInstance().getBestCandidates()) {
+							if ((filter.get(c.getBranch()) || filter.get(c.getSpeciality()))
+									&& (c.getCid().toLowerCase().contains(query)
+											|| c.getName().toLowerCase().contains(query)
+											|| c.getAddress().toLowerCase().contains(query)
+											|| c.getPhone().toLowerCase().contains(query)
+											|| c.getSchoolLevel().toLowerCase().contains(query)
+											|| c.getSpeciality().toLowerCase().contains(query))) {
+								table.addRow(new Object[] { c.getCid(), c.getBranch(), c.getName(), c.getSex(),
+										c.getAddress(), c.getPhone(), c.getSchoolLevel(), c.getSpeciality(),
+										c.getXpYears() });
+							}
+						}
+					}
+				});
+
+		panelReport1.render();
+		panelReport2.render();
+		panelReport3.render();
+		panelReport4.render();
+		contentPane.add(panelReport1);
+		contentPane.add(panelReport2);
+		contentPane.add(panelReport3);
+		contentPane.add(panelReport4);
+
+		switchPanel(panelReport1);
+	}
+
+	private void switchPanel(JPanel panelToShow) {
+		panelReport1.setVisible(false);
+		panelReport2.setVisible(false);
+		panelReport3.setVisible(false);
+		panelReport4.setVisible(false);
+		panelToShow.setVisible(true);
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public ReportsHomeScreen() {
-		final Agency agency = GlobalAgency.getInstance();
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 935, 495);
@@ -56,60 +178,7 @@ public class ReportsHomeScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(288, 11, 609, 434);
-		contentPane.add(panel_1);
-
-		final JLabel titleTab = new JLabel("Mejores Ofertas");
-		titleTab.setHorizontalAlignment(SwingConstants.CENTER);
-		titleTab.setBounds(175, 11, 264, 26);
-		titleTab.setBorder(UIManager.getBorder("EditorPane.border"));
-		titleTab.setFont(new Font("Tahoma", Font.PLAIN, 21));
-
-		final DefaultTableModel mejoresOfertasModel = new DefaultTableModel(new Object[][] { { null, null, null }, },
-				new String[] { "Compañía", "Rama", "Salario" });
-
-		for (Offer offer : agency.getBestOffers()) {
-			mejoresOfertasModel
-					.addRow(new Object[] { agency.getCompanyManager().getCompanyById(offer.getCompanyId()).getName(),
-							offer.getBranch(), offer.getSalary() + "" });
-		}
-
-		final DefaultTableModel entrevistasPorDiaModel = new DefaultTableModel(
-				new Object[][] { { null, null, null, null }, }, new String[] { "Candidato", "Mes", "Día", "Oferta" });
-
-		for (Candidate candidate : agency.getCandidates()) {
-			for (Interview interview : agency.getInterviewsByCandidate(candidate.getCid())) {
-
-				entrevistasPorDiaModel
-						.addRow(new Object[] { agency.getCandidateByCid(interview.getCandidateCid()).getName(),
-								interview.getMonthlyId(), interview.getDayId(), interview.getOfferId(), });
-			}
-		}
-		final DefaultTableModel mayoresEmpleadoresModel = new DefaultTableModel(new Object[][] { { null, null, null,null,null }, },
-				new String[] { "ID","Nombre","Direccion", "Telefono", "Sector" });
-		
-		for (Company c: GlobalAgency.getInstance().getCompaniesMostOffers()){
-			
-			mayoresEmpleadoresModel
-				.addRow(new Object[] {c.getId(),c.getName(),c.getAddress(),c.getPhone(),c.getSector()});
-		}
-		final DefaultTableModel candidatosDestacadosModel = new DefaultTableModel(new Object[][] { { null, null, null,null,null,null,null,null,null }, },
-				new String[] { "Cid","Rama","Nombre", "Sexo", "Direccion","Telefono","Escolaridad","Especialidad","Experiencia" });
-		for (Candidate c: GlobalAgency.getInstance().getBestCandidates()){
-			candidatosDestacadosModel.addRow(new Object[]{c.getCid(),c.getBranch(),c.getName(),c.getSex(),c.getAddress(),c.getPhone(),c.getSchoolLevel(),c.getSpeciality(),c.getXpYears()});
-		}
-			
-			
-
-		table = new MTable(mejoresOfertasModel);
-		
-		
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 76, 589, 347);
-		panel_1.add(scrollPane);
-		scrollPane.setViewportView(table);
+		initializePanels();
 
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 102, 255));
@@ -120,8 +189,7 @@ public class ReportsHomeScreen extends JFrame {
 		final JButton btnMejoresOfertas = new JButton("Mejores Ofertas");
 		btnMejoresOfertas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				titleTab.setText(btnMejoresOfertas.getText());
-				table.setModel(mejoresOfertasModel);
+				switchPanel(panelReport1);
 			}
 		});
 		btnMejoresOfertas.setContentAreaFilled(false);
@@ -130,60 +198,13 @@ public class ReportsHomeScreen extends JFrame {
 		btnMejoresOfertas.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnMejoresOfertas.setBounds(10, 92, 248, 43);
 		panel.add(btnMejoresOfertas);
-		panel_1.setLayout(null);
 
-		panel_1.add(titleTab);
-
-		btnSearch = new JTextField();
-		btnSearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Company company = null;
-				ArrayList<Offer> offersByCompany = new ArrayList<Offer>();
-				for (Company c : agency.getCompanyManager().getCompanies())
-					if (c.getName().equalsIgnoreCase(btnSearch.getText()))
-						company = c;
-				for (Offer of : company.getOffers())
-					for (Offer o : agency.getBestOffers())
-						if (of.compareTo(o))
-							offersByCompany.add(o);
-				mejoresOfertasModel.setRowCount(0);
-				for (Offer o : offersByCompany)
-					mejoresOfertasModel.addRow(new Object[] { o.getCompanyId(), o.getBranch(), o.getSalary() });
-
-			}
-		});
-		btnSearch.setBounds(10, 50, 179, 20);
-		panel_1.add(btnSearch);
-		btnSearch.setColumns(10);
-
-		JLabel lblBuscar = new JLabel("Buscar");
-		lblBuscar.setBounds(10, 33, 46, 14);
-		panel_1.add(lblBuscar);
-
-		JButton button = new JButton("");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				mejoresOfertasModel.setRowCount(0);
-				for (Offer offer : agency.getBestOffers()) {
-					mejoresOfertasModel.addRow(
-							new Object[] { agency.getCompanyManager().getCompanyById(offer.getCompanyId()).getName(),
-									offer.getBranch(), offer.getSalary() + "" });
-				}
-			}
-		});
-		button.setIcon(new ImageIcon(
-				ReportsHomeScreen.class.getResource("/com/sun/javafx/scene/control/skin/modena/dialog-error.png")));
-		button.setBounds(185, 50, 37, 20);
-		panel_1.add(button);
-
-		final JButton btnEntrevistasPorDia = new JButton("Entrevistas por dia");
+		final JButton btnEntrevistasPorDia = new JButton("Entrevistas por día");
 		btnEntrevistasPorDia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				titleTab.setText(btnEntrevistasPorDia.getText());
-				table.setModel(entrevistasPorDiaModel);
+				switchPanel(panelReport2);
 			}
 		});
-
 		btnEntrevistasPorDia.setForeground(Color.WHITE);
 		btnEntrevistasPorDia.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnEntrevistasPorDia.setContentAreaFilled(false);
@@ -194,8 +215,7 @@ public class ReportsHomeScreen extends JFrame {
 		final JButton btnReport3 = new JButton("Mayores Empleadores");
 		btnReport3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				titleTab.setText(btnReport3.getText());
-				table.setModel(mayoresEmpleadoresModel);
+				switchPanel(panelReport3);
 			}
 		});
 		btnReport3.setForeground(Color.WHITE);
@@ -208,8 +228,7 @@ public class ReportsHomeScreen extends JFrame {
 		final JButton btnReport4 = new JButton("Candidatos Destacados");
 		btnReport4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				titleTab.setText(btnReport4.getText());
-				table.setModel(candidatosDestacadosModel);
+				switchPanel(panelReport4);
 			}
 		});
 		btnReport4.setForeground(Color.WHITE);
@@ -223,7 +242,7 @@ public class ReportsHomeScreen extends JFrame {
 		String[] columnNames = { "Nombre", "Dirección", "Teléfono", "Sector" };
 		tableModel = new DefaultTableModel(columnNames, 1);
 		tableModel.addRow(new Object[] { "w", "ww", "ooo", "rtt" });
-		
+
 		//
 		// Boton para regresar
 		//
