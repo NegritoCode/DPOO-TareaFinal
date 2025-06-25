632a35ae8963f2f2114cbdfaef62fc02882fb0fc
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-06-25 12:02:22 -0400

    feat: simplified id generation

--- src/utils/Id.java
+++ src/utils/Id.java
@@ -1,10 +1,24 @@
 package utils;
 
+import java.util.Random;
+
 public class Id {
     private static long counter = 1;
 
     public static String generateId(String prefix) {
-        return prefix + "_" + System.currentTimeMillis() + "_" + (counter++);
+        String randomLetters = generateRandomLetters(2);
+        return prefix + "_" + (counter++) + randomLetters;
+    }
+
+    private static String generateRandomLetters(int length) {
+        Random random = new Random();
+        StringBuilder letters = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            // esto genera una letra aleatoria entre A y Z
+            char letter = (char) ('A' + random.nextInt(26));
+            letters.append(letter);
+        }
+        return letters.toString();
     }
     
     public static String getMonth(int monthIndex) {
